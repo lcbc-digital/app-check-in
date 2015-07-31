a02f794ce03333d9ac6132e4140a32b4f70c58ce
Justin Eveland <jbeveland27@gmail.com>
2015-07-30 21:51:15 -0400

    Allow Signup and use SSL with Puma webserver in Production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	# Returns the full title on a per-page basis.
+	def full_title(page_title = '')
+	  base_title = "LCBC Mobile Check-In"
+	  if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+	  end
+	end
+
 end
