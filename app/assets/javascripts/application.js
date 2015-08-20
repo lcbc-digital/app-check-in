e4a4d01d441384a13e7806555ed38bcc5cd00140
Justin Eveland <jbeveland27@gmail.com>
2015-08-20 01:31:56 -0400

    Remove jquery-ui elements

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,10 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= bootstrap
-//= require jquery-ui/selectmenu
 //= require turbolinks
 //= require_tree .
 
-$(function() {
-  $('.selectmenu').selectmenu();
-});
