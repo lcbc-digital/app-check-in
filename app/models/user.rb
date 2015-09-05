46f1755a2fa9d65fcc4be058a227db068e655090
Justin Eveland <jbeveland27@gmail.com>
2015-09-05 19:15:59 -0400

    Fixed Attendance View Bug where nil DOB broke view

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, length: { maximum: 255 },
 										format: { with: VALID_EMAIL_REGEX },
 										uniqueness: { case_sensitive: false }
+	validates :date_of_birth, presence: true
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
