7047deb7caf8f321487ddfd9efc87c9489189b4b
Justin Eveland <jbeveland27@gmail.com>
2015-09-05 15:11:33 -0400

    Styling updates and confirmation page update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      flash[:success] = "Profile updated"
+      flash[:success] = "Success - profile updated."
       redirect_to check_in_path
     else
       render 'edit'
