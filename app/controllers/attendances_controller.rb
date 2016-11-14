d7e6d170a09a9c7a48d4d74ebddccd9cef6073b2
Josh Kennedy <joshkennedy@lcbcchurch.com>
2016-11-14 08:19:13 -0500

    add attendance csv download option

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,5 +1,15 @@
 class AttendancesController < ApplicationController
   def new
-  	@checkins = CheckIn.includes('user').all
+    @checkins = CheckIn.includes('user').all
+
+    respond_to do |format|
+      format.html
+      # format.csv { send_data @checkins.to_csv, filename: "checkins-#{Date.today}.csv" }
+      format.csv do
+        send_data @checkins.to_csv
+        response.headers['Content-Type'] = 'text/csv'
+        response.headers['Content-Disposition'] = "attachment; filename=checkins-#{Date.today}.csv"
+      end
+    end
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
+require 'csv'
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -24,6 +25,8 @@ module LcbcCheckIn
     config.active_record.raise_in_transactional_callbacks = true
 
     # Serve static assets
-    config.serve_static_assets = true
+    config.serve_static_files = true
+
+
   end
 end
